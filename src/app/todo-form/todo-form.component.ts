59f80d39f665841587c51281eb56ad49f011bb52
DaedalusAlpha <mattrmcc@gmail.com>
2022-10-06 11:40:49 -0500

    modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts 	new file:   src/app/todo-form/todo-form.component.css 	new file:   src/app/todo-form/todo-form.component.html 	new file:   src/app/todo-form/todo-form.component.spec.ts 	new file:   src/app/todo-form/todo-form.component.ts 	new file:   src/app/todo-item/todo-item.component.css 	new file:   src/app/todo-item/todo-item.component.html 	new file:   src/app/todo-item/todo-item.component.spec.ts 	new file:   src/app/todo-item/todo-item.component.ts 	new file:   src/app/todo-list/todo-list.component.css 	new file:   src/app/todo-list/todo-list.component.html 	new file:   src/app/todo-list/todo-list.component.spec.ts 	new file:   src/app/todo-list/todo-list.component.ts 	new file:   src/app/todo.ts 	modified:   src/index.html 	modified:   src/styles.css

---
+++ src/app/todo-form/todo-form.component.ts
@@ -0,0 +1,27 @@
+import { Component, Output, OnInit, EventEmitter } from '@angular/core';
+import { Todo } from '../todo';
+
+@Component({
+  selector: 'app-todo-form',
+  templateUrl: './todo-form.component.html',
+  styleUrls: ['./todo-form.component.css'],
+})
+export class TodoFormComponent implements OnInit {
+  todo: Todo = {} as Todo;
+  @Output() addTodo = new EventEmitter<Todo>();
+
+  addNewTodo(): void {
+    if (this.todo.task == null || this.todo.task == '') {
+      alert('Task is required');
+    } else {
+      let newTodo: Todo = {} as Todo;
+      newTodo.completed = false;
+      newTodo.task = this.todo.task;
+      this.addTodo.emit(newTodo);
+      this.todo = {} as Todo;
+    }
+  }
+
+  constructor() {}
+  ngOnInit(): void {}
+}
---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../todo';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css'],
+})
+export class TodoListComponent implements OnInit {
+  todoFilter: string = '';
+  todos: Todo[] = [
+    {
+      task: 'Take out the trash',
+      completed: false,
+    },
+    {
+      task: 'Get groceries',
+      completed: true,
+    },
+    {
+      task: 'Watch LotR extended edition',
+      completed: true,
+    },
+    {
+      task: 'Watch LotR extended edition again',
+      completed: false,
+    },
+    {
+      task: 'Finish this assignment',
+      completed: false,
+    },
+  ];
+
+  // Function to remove a todo from the array.
+  removeTodo(index: number): void {
+    this.todos.splice(index, 1);
+  }
+
+  // Function to mark a todo item as complete.
+  completeTodo(index: number): void {
+    this.todos[index].completed = true;
+  }
+
+  // Function to add a todo item to the array.
+  newTodo(t: Todo): void {
+    this.todos.push(t);
+  }
+  constructor() {}
+
+  // Check to see if the array of todos is either empty
+  // or if every task is complete.
+  checkComplete(): boolean {
+    let result: boolean = false;
+    if (this.todos.length == 0 || this.todos.every((t) => t.completed)) {
+      result = true;
+    }
+    return result;
+  }
+
+  // Check if the todo item has text that matches the filter.
+  checkFilter(t: string): boolean {
+    if (t.toLowerCase().includes(this.todoFilter.toLowerCase())) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  ngOnInit(): void {}
+}
